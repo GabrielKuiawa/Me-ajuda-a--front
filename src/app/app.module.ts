33fa415ebdb056ea5128d816fbc2e580159c63d8
GabrielKuiawa <gabrielsantos77239@gmail.com>
2022-11-24 01:13:54 -0300

    fazendo auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+    ChatComponent,
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
     AppRoutingModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDDW1-bzo0q0_5StDfZ_5c_3IGGAYknTRU",
+    authDomain: "me-ajuda-ae-664ca.firebaseapp.com",
+    projectId: "me-ajuda-ae-664ca",
+    storageBucket: "me-ajuda-ae-664ca.appspot.com",
+    messagingSenderId: "480215446284",
+    appId: "1:480215446284:web:03cf53ab58d8aec42b8f29",
+    measurementId: "G-WY38F96PZW"
+  }
 };
 
 /*
