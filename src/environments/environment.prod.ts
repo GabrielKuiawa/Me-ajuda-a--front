58a81f16c4e9de42a40d059e6c62173b1ff47c24
GabrielKuiawa <gabrielsantos77239@gmail.com>
2022-11-24 09:29:38 -0300

    para produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDDW1-bzo0q0_5StDfZ_5c_3IGGAYknTRU",
+    authDomain: "me-ajuda-ae-664ca.firebaseapp.com",
+    projectId: "me-ajuda-ae-664ca",
+    storageBucket: "me-ajuda-ae-664ca.appspot.com",
+    messagingSenderId: "480215446284",
+    appId: "1:480215446284:web:03cf53ab58d8aec42b8f29",
+    measurementId: "G-WY38F96PZW"
+  }
 };
